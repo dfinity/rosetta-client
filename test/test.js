9ac7d86e1a015224049c5bebfdf2c3ea6bd0191b
Cheng Shao <cheng.shao@tweag.io>
2021-02-26 17:43:36 +0000

    WIP

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const { inspect } = require("util");
   try {
     const submit_res = await session.transfer(
       hex_decode(
-        "093c3e2191be336f246259769041dd75b326143746b2ca97cb0f66273a366ba5ae7c3e96d49d7e5b1f74ce1e8ff640957c3ba4d7199f463a9fcff4c68b19f5e4"
+        "093c3e2191be336f246259769041dd75b326143746b2ca97cb0f66273a366ba5ae7c3e96d49d7e5b1f74ce1e8ff640957c3ba4d7199f463a9fcff4c68b19f5e3"
       ),
       hex_decode("df11fdc42d372cea555c5b1c0e73c67ea103d1e8a4d0e669a0b9d4ac02"),
       1000000n
