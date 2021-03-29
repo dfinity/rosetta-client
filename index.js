c5ecf2f50aa1a7e98b75acbf02e9a4de71ad6bc3
Cheng Shao <cheng.shao@tweag.io>
2021-03-29 09:51:20 +0200

    Use tweetnacl for ed25519 logic and recover previous interface (#17)

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const { blobFromHex, blobToHex } = require("@dfinity/agent");
-const {
-  Ed25519PublicKey,
-  Ed25519KeyIdentity,
-} = require("@dfinity/authentication");
 
 // Use specific export syntax to allow bundlers and IDEs to properly load dependencies.
 module.exports = {
   blobFromHex: blobFromHex,
   blobToHex: blobToHex,
-  Ed25519PublicKey: Ed25519PublicKey,
-  Ed25519KeyIdentity: Ed25519KeyIdentity,
   ...require("./lib/chain"),
   ...require("./lib/construction_combine"),
   ...require("./lib/key"),
-  ...require("./lib/patch"),
   ...require("./lib/session"),
   ...require("./lib/stats"),
   ...require("./lib/transaction_hash"),
--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
 const { inspect } = require("util");
 const {
-  Ed25519KeyIdentity,
   blobFromHex,
   address_from_hex,
   Chain,
   Session,
-} = require("../index.js");
+} = require("../index");
 
 (async () => {
   const session = new Session({ baseUrl: "http://localhost:8080" });
@@ -13,10 +12,8 @@ const {
 
   try {
     let submit_res = await session.transfer(
-      Ed25519KeyIdentity.fromSecretKey(
-        blobFromHex(
-          "093c3e2191be336f246259769041dd75b326143746b2ca97cb0f66273a366ba5ae7c3e96d49d7e5b1f74ce1e8ff640957c3ba4d7199f463a9fcff4c68b19f5e3"
-        )
+      blobFromHex(
+        "093c3e2191be336f246259769041dd75b326143746b2ca97cb0f66273a366ba5ae7c3e96d49d7e5b1f74ce1e8ff640957c3ba4d7199f463a9fcff4c68b19f5e3"
       ),
       address_from_hex(
         "1e1838071cb875e59c1da64af5e04951bb3c1e94c1285bf9ff7480a645e1aa56"
